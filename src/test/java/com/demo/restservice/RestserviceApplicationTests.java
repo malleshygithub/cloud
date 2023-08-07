13308dc6874f54c2e1e6e9f5f77eeb05aecf91a6
malleshygithub <108399686+malleshygithub@users.noreply.github.com>
2023-08-07 12:07:21 +0530

    comment out the TESTS RestserviceApplicationTests.java

--- src/test/java/com/demo/restservice/RestserviceApplicationTests.java
+++ src/test/java/com/demo/restservice/RestserviceApplicationTests.java
@@ -3,10 +3,10 @@ package com.demo.restservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RestserviceApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
