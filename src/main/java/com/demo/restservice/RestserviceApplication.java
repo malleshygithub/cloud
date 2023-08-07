77b87c5edd24b692bb194720a5f627d024a50579
malleshygithub <108399686+malleshygithub@users.noreply.github.com>
2023-08-07 13:07:37 +0530

    Update RestserviceApplication.java

--- src/main/java/com/demo/restservice/RestserviceApplication.java
+++ src/main/java/com/demo/restservice/RestserviceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@SpringBootApplication
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class},scanBasePackages={
+"com.demo.restservice.repository"})
+	
 public class RestserviceApplication {
 
 	public static void main(String[] args) {
